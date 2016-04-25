eb6a1bac7e4fb187b1195926e520d919a35b1085
tormen <mentux@gmail.com>
2016-04-25 20:56:11 -0300

    Adicao de itens refatorados.

--- webservice01/nusoap/cliente.php
+++ webservice01/nusoap/cliente.php
@@ -4,18 +4,21 @@
 // Baixar o arquivo nusoap.php
 
 // inclusao do arquivo de classes NuSOAP
-require_once('lib/nusoap.php');
-include_once('../Constants.php');
+require_once 'lib/nusoap.php';
+
 // criacao de uma instancia do cliente
-$client = new soapclient(Constants::URL_LOCAL_NUSOAP); // ex.: http://localhost/nusoap/server.php
+try {
+    $client = new nusoap_client('http://localhost/pdw/pdw-ws/webservice01/nusoap/server.php');
+} catch (Exception $e) {
+    echo get_class($e).' thrown within the exception handler. <hr> Message: '.$e->getMessage().' on line '.$e->getLine();
+}
 
 // chamada do m�todo SOAP
-$result = $client->call('helloWorld',array('Tormen'));
+$result = $client->call('helloWorld', array('Tormen'));
 // exibe o resultado
 print_r($result);
 
 // OPCIONAL : exibe a requisicao e a resposta
-
 
 echo '<h2>Requisicao</h2>';
 echo '<pre>'.htmlspecialchars($client->request).'</pre>';
@@ -24,5 +27,3 @@ echo '<pre>'.htmlspecialchars($client->response).'</pre>';
 // Exibe mensagens para debug
 echo '<h2>Debug</h2>';
 echo '<pre>'.htmlspecialchars($client->debug_str).'</pre>';
-/**/
-?>
--- webservice01/nusoap/cliente2.php
+++ webservice01/nusoap/cliente2.php
@@ -3,34 +3,36 @@
 // Comentar a extensao php_soap
 // Baixar o arquivo nusoap.php
 
-require_once('lib/nusoap.php');
-include_once('../Constants.php');
+require_once 'lib/nusoap.php';
+include_once '../Constants.php';
 
 // Definicao da localizacao do arquivo WSDL
 $wsdl = Constants::URL_LOCAL_NUSOAP_WSDL; // ex.: http://localhost/nusoap/server2.php?wsdl
-
 // criacao de uma instancia do cliente
-$client = new soapclient($wsdl, true);
+try {
+    $client = new nusoap_client($wsdl);//, true);
+    // verifica se ocorreu erro na criacao do objeto
+} catch (Exception $e) {
+    echo get_class($e).' thrown within the exception handler. Message: '.$e->getMessage().' on line '.$e->getLine();
+}
 
-// verifica se ocorreu erro na criacao do objeto
 $err = $client->getError();
-if ($err){
-	echo "Erro no construtor<pre>".$err."</pre>";
+if ($err) {
+    echo 'Erro no construtor<pre>'.$err.'</pre>';
 }
 
 // chamada do metodo SOAP
-$result = $client->call('helloWorld',array('Tormen'));
-
+$result = $client->call('helloWorld', array('Tormen'));
 // verifica se ocorreu falha na chamada do metodo
-if ($client->fault){
-	echo "Falha<pre>".print_r($result)."</pre>";
-}else{
-	// verifica se ocorreu erro
-	$err = $client->getError();
-	if ($err){
-		echo "Erro<pre>".$err."</pre>";
-	} else{
-			print_r($result);
-	}//end_else
+if ($client->fault) {
+    echo 'Falha<pre>'.print_r($result).'</pre>';
+} else {
+    // verifica se ocorreu erro
+    $err = $client->getError();
+    if ($err) {
+        echo 'Erro<pre>'.$err.'</pre>';
+    } else {
+        print_r($result);
+    }//end_else
 }//end_else
-?>
+;
